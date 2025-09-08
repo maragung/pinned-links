ecb3f973abe1f4a01c4a7047201204a883d715d6
Maragung <50156713+maragung@users.noreply.github.com>
2025-09-08 10:47:40 +0700

    Update route.js

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -18,7 +18,20 @@ export async function GET() {
       label: "Mycel",
       url: "https://discord.com/channels/1082271726613839892/1138928858926678066",
     },
+    {
+      label: "Pharos",
+      url: "https://discord.com/channels/1270276651636232282/1306921018509754399"
+    },
+    {
+      label: "Rialo",
+      url: "https://discord.com/channels/1393211102577557544/1400494188054708286"
+    },
+    {
+      label: "Seismic",
+      url: "https://discord.com/channels/1343751435711414362/1343751437087150113"
+    }
   ];
 
   return NextResponse.json({ links }, { status: 200 });
 }
+
